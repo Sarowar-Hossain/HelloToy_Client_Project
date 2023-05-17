8acc17bc9ca34d8ad2f7db1a732dea4a81168775
Sarowar-Hossain <mdsarowar1812@gmail.com>
2023-05-18 00:27:59 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>hello from hello toys</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+    </>
+  );
+}
+
+export default App;
