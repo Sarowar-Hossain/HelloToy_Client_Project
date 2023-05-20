cb345e3adff19937e5adced83c97f7569c89c876
Sarowar-Hossain <mdsarowar1812@gmail.com>
2023-05-20 16:06:03 +0800

    client section add

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -5,23 +5,18 @@ import ShopByCategory from "../../ShopByCategory/ShopByCategory";
 import HotOffer from "../../HotOffer/HotOffer";
 import { UserContext } from "../../../Context/Context";
 import { TailSpin } from "react-loader-spinner";
+import ClientsReview from "../../Clients/ClientsReview";
 
 const Home = () => {
   const { user, spinner, setTitle } = useContext(UserContext);
   setTitle("Home");
   return (
     <div>
-      {/* {user ? (
-        <> </>
-      ) : (
-        <>
-          <div className="flex justify-center items-center">{spinner()}</div>
-        </>
-      )} */}
       <Banner></Banner>
       <Gallary></Gallary>
       <ShopByCategory></ShopByCategory>
       <HotOffer></HotOffer>
+      <ClientsReview></ClientsReview>
     </div>
   );
 };
